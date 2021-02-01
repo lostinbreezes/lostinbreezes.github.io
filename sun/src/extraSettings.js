dc4f5a23f0a5a804f6b4cf37c32875ec420912e0
sere <lostinbreezes@users.noreply.github.com>
2021-02-01 19:31:54 +0800

    1

---
+++ sun/src/extraSettings.js
@@ -0,0 +1,18 @@
+// 额外分数：修改数字
+let extraScore = 1;
+
+// 无敌模式：true 改为 false
+let wuDi = true;
+
+// 第一个水果：修改数字为 0-10
+let firstFruit = 0;
+
+// 让水果更 Q 弹：false 改为大于 0 小于 1 的任意小数（推荐 0.9）
+let fruitQTan = false;
+
+// 让水果下落缓慢：false 改为大于 0 的任意数，值越大阻力越大，下落越慢（推荐 5）
+let fruitSlowDown = false;
+
+
+// 开启弹窗，暂不支持
+let selectModal = false;
